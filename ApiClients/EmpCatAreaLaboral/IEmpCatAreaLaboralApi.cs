14d07153c2590d8e3158ad792d4b06be72b362e7
Peeprs <axelcrewsegundocrew"gmail.com>
2026-07-01 16:36:46 -0600

    fix: Se quito todo el codigo harcodeado de todas las nuevas pantallas

--- ApiClients/EmpCatAreaLaboral/IEmpCatAreaLaboralApi.cs
+++ ApiClients/EmpCatAreaLaboral/IEmpCatAreaLaboralApi.cs
@@ -6,29 +6,29 @@ namespace Cavex.Principal.ApiClients.EmpCatAreaLaboral
 {
     public interface IEmpCatAreaLaboralApi
     {
-        [Get("/api/EmpCatAreaLaboral")]
+        [Get("/api/v1/EmpCatAreaLaboral")]
         Task<ResponseWrapper<PagedResponse<EmpCatAreaLaboralDto>>> GetAllAsync(
             [Body] RequestWrapper<EmpCatAreaLaboralQueryDto> request,
             CancellationToken cancellationToken = default);
 
-        [Get("/api/EmpCatAreaLaboral/{id}")]
+        [Get("/api/v1/EmpCatAreaLaboral/{id}")]
         Task<ResponseWrapper<EmpCatAreaLaboralDto>> GetByIdAsync(
             int id,
             [Body] RequestWrapper<int> request,
             CancellationToken cancellationToken = default);
 
-        [Post("/api/EmpCatAreaLaboral")]
+        [Post("/api/v1/EmpCatAreaLaboral")]
         Task<ResponseWrapper<EmpCatAreaLaboralDto>> CreateAsync(
             [Body] RequestWrapper<EmpCatAreaLaboralSaveDto> request,
             CancellationToken cancellationToken = default);
 
-        [Put("/api/EmpCatAreaLaboral/{id}")]
+        [Put("/api/v1/EmpCatAreaLaboral/{id}")]
         Task<ResponseWrapper<EmpCatAreaLaboralDto>> UpdateAsync(
             int id,
             [Body] RequestWrapper<EmpCatAreaLaboralSaveDto> request,
             CancellationToken cancellationToken = default);
 
-        [Delete("/api/EmpCatAreaLaboral/{id}")]
+        [Delete("/api/v1/EmpCatAreaLaboral/{id}")]
         Task<ResponseWrapper<bool>> DeleteAsync(
             int id,
             [Body] RequestWrapper<int> request,
